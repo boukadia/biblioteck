a1d330330d9867a0e1fe453ac7b5b9a7f49acd3b
boukadia <mustapha.boukadia97@gmail.com>
2026-03-02 10:53:17 +0000

    BIB-61 Changer le Statut :

--- BACKEND/prisma/seed.ts
+++ BACKEND/prisma/seed.ts
@@ -1,111 +1,111 @@
-// import { PrismaClient } from '../generated/prisma';
-
-import { PrismaClient } from 'generated/prisma/client';
+// // import { PrismaClient } from '../generated/prisma';
 
 // import { PrismaClient } from 'generated/prisma/client';
 
-// import { PrismaClient } from "@prisma/client/scripts/default-index.js";
-// import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+// // import { PrismaClient } from 'generated/prisma/client';
 
-async function main() {
-  console.log('🌱 Start seeding...');
+// // import { PrismaClient } from "@prisma/client/scripts/default-index.js";
+// // import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
 
-  // 1. Créer les Récompenses (Boutique)
-  const rewards = [
-    {
-      nom: 'Time Boost',
-      description: 'Ajoute 7 jours à votre emprunt',
-      cout: 50,
-      type: 'TIME_BOOST',
-    },
-    {
-      nom: 'Immunity',
-      description: 'Gèle les pénalités pendant 3 jours',
-      cout: 100,
-      type: 'IMMUNITY',
-    },
-    {
-      nom: 'Pass VIP',
-      description: 'Empruntez jusqu’à 5 livres pendant 30 jours',
-      cout: 300,
-      type: 'PREMIUM_PASS',
-    },
-    {
-      nom: 'Extra Slot',
-      description: 'Un livre supplémentaire pour cet emprunt',
-      cout: 150,
-      type: 'EXTRA_SLOT',
-    },
-    {
-      nom: 'Account Reset',
-      description: 'Efface vos dettes et vous débloque',
-      cout: 500,
-      type: 'ACCOUNT_RESET',
-    },
-  ];
+// async function main() {
+//   console.log('🌱 Start seeding...');
 
-  for (const r of rewards) {
-    await prisma.recompense.upsert({
-      where: { id: rewards.indexOf(r) + 1 }, // Pour éviter les doublons
-      update: {},
-      create: r as any,
-    });
-  }
+//   // 1. Créer les Récompenses (Boutique)
+//   const rewards = [
+//     {
+//       nom: 'Time Boost',
+//       description: 'Ajoute 7 jours à votre emprunt',
+//       cout: 50,
+//       type: 'TIME_BOOST',
+//     },
+//     {
+//       nom: 'Immunity',
+//       description: 'Gèle les pénalités pendant 3 jours',
+//       cout: 100,
+//       type: 'IMMUNITY',
+//     },
+//     {
+//       nom: 'Pass VIP',
+//       description: 'Empruntez jusqu’à 5 livres pendant 30 jours',
+//       cout: 300,
+//       type: 'PREMIUM_PASS',
+//     },
+//     {
+//       nom: 'Extra Slot',
+//       description: 'Un livre supplémentaire pour cet emprunt',
+//       cout: 150,
+//       type: 'EXTRA_SLOT',
+//     },
+//     {
+//       nom: 'Account Reset',
+//       description: 'Efface vos dettes et vous débloque',
+//       cout: 500,
+//       type: 'ACCOUNT_RESET',
+//     },
+//   ];
 
-  // 2. Créer quelques Livres de test
-  const books = [
-    {
-      titre: 'Clean Code',
-      auteur: 'Robert C. Martin',
-      isbn: '978-0132350884',
-      categorie: 'Informatique',
-      stock: 5,
-    },
-    {
-      titre: 'The Pragmatic Programmer',
-      auteur: 'Andrew Hunt',
-      isbn: '978-0201616224',
-      categorie: 'Informatique',
-      stock: 3,
-    },
-    {
-      titre: 'L’Alchimiste',
-      auteur: 'Paulo Coelho',
-      isbn: '978-2290004449',
-      categorie: 'Littérature',
-      stock: 10,
-    },
-  ];
+//   for (const r of rewards) {
+//     await prisma.recompense.upsert({
+//       where: { id: rewards.indexOf(r) + 1 }, // Pour éviter les doublons
+//       update: {},
+//       create: r as any,
+//     });
+//   }
 
-  for (const b of books) {
-    await prisma.livre.upsert({
-      where: { isbn: b.isbn },
-      update: {},
-      create: b,
-    });
-  }
+//   // 2. Créer quelques Livres de test
+//   const books = [
+//     {
+//       titre: 'Clean Code',
+//       auteur: 'Robert C. Martin',
+//       isbn: '978-0132350884',
+//       categorie: 'Informatique',
+//       stock: 5,
+//     },
+//     {
+//       titre: 'The Pragmatic Programmer',
+//       auteur: 'Andrew Hunt',
+//       isbn: '978-0201616224',
+//       categorie: 'Informatique',
+//       stock: 3,
+//     },
+//     {
+//       titre: 'L’Alchimiste',
+//       auteur: 'Paulo Coelho',
+//       isbn: '978-2290004449',
+//       categorie: 'Littérature',
+//       stock: 10,
+//     },
+//   ];
 
-  // 3. Créer un Admin de test
-  await prisma.utilisateur.upsert({
-    where: { email: 'admin@bibliotech.com' },
-    update: {},
-    create: {
-      nom: 'Admin Biblio',
-      email: 'admin@bibliotech.com',
-      motDePasse: 'admin123', // À hasher plus tard !
-      role: 'ADMIN',
-    },
-  });
+//   for (const b of books) {
+//     await prisma.livre.upsert({
+//       where: { isbn: b.isbn },
+//       update: {},
+//       create: b,
+//     });
+//   }
 
-  console.log('✅ Seeding finished.');
-}
+//   // 3. Créer un Admin de test
+//   await prisma.utilisateur.upsert({
+//     where: { email: 'admin@bibliotech.com' },
+//     update: {},
+//     create: {
+//       nom: 'Admin Biblio',
+//       email: 'admin@bibliotech.com',
+//       motDePasse: 'admin123', // À hasher plus tard !
+//       role: 'ADMIN',
+//     },
+//   });
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+//   console.log('✅ Seeding finished.');
+// }
+
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
--- BACKEND/src/users/users.service.ts
+++ BACKEND/src/users/users.service.ts
@@ -1,16 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { log } from 'node:console';
+import { Prisma } from 'src/prisma/entities/prisma.entity';
+import { StatutUtilisateur } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
+  constructor (private readonly prisma: PrismaService){}
 
 
   findAll() {
-    return `This action returns all users`;
+    // if (user.role!=="ADMIN") {
+    //   throw BadRequestException("admin required");
+    // }
+    
+    const users= this.prisma.utilisateur.findMany()
+    return  users;
   }
-  async changeStatus() {
-    const 
+  async changeStatus(id: number) {
+    const user =await this.prisma.utilisateur.findUnique({where:{
+      id:id
+    }})
+    if (!user) {
+    throw new BadRequestException("user not found")
+    }
+   if (user.statut=== StatutUtilisateur.ACTIF) {
+    const updatedUser=await this.prisma.utilisateur.update({
+      where:{
+        id: id
+      },
+      data: {
+        statut:'BLOQUE',
+      }
+    })
+     return updatedUser
+    }
+    else {
+      const updatedUser=await this.prisma.utilisateur.update({
+        where:{
+          id:id
+        },
+        data:{
+          statut:'ACTIF'
+        },
+
+      })
+      return updatedUser
+      
+
+   
+  }
   }
 
   findOne(id: number) {
