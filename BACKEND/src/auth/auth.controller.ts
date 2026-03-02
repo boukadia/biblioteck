c3f19e8e03585dd7d5069205c0fa4a61e9ee0e7a
boukadia <mustapha.boukadia97@gmail.com>
2026-03-02 14:30:09 +0000

    BIB-65 Implémenter le CRUD Utilisateurs (Inscription/Connexion).

--- BACKEND/src/auth/auth.controller.ts
+++ BACKEND/src/auth/auth.controller.ts
@@ -2,33 +2,19 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginDto } from './dto/Login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @Post('register')
+  register(@Body() data: CreateAuthDto){
+    return this.authService.register(data)
+  }
+  @Post('login')
+  login(@Body() data: LoginDto){
+    return this.authService.login(data)
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
 }
