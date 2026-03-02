6eb8f1f5c07f1292f07d5b51e5af52e0fad60405
boukadia <mustapha.boukadia97@gmail.com>
2026-03-02 14:30:09 +0000

    BIB-65 Implémenter le CRUD Utilisateurs (Inscription/Connexion).

--- BACKEND/src/auth/auth.service.ts
+++ BACKEND/src/auth/auth.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadGatewayException,
+  BadRequestException,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { log } from 'console';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(
+    private readonly prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  async register(data: any) {
+    const user = await this.prisma.utilisateur.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+    if (user) {
+      throw new BadRequestException('email already exists');
+    }
+    const hashedPassword = await bcrypt.hash(data.motDePasse, 10);
+    const newUser = await this.prisma.utilisateur.create({
+      data: {
+        ...data,
+        motDePasse: hashedPassword,
+      },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+    const payload = {
+      sub: newUser.id,
+      email: newUser.email,
+      role: newUser.role,
+    };
+    const access_token = await this.jwtService.signAsync(payload);
+    return { token: access_token };
   }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async login(data) {
+    const user = await this.prisma.utilisateur.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+    if (!user) {
+      throw new BadRequestException('invalid email');
+    }
+    const isPassword = await bcrypt.compare(data.motDePasse, user.motDePasse);
+    if (!isPassword) {
+      throw new BadRequestException('invalid password');
+    }
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      role: user.role,
+    };
+    const access_token = await this.jwtService.signAsync(payload);
+    const { motDePasse, ...safeUser } = user; //supprimer mots du pass dans response
+    return { token: access_token, user: safeUser };
   }
 }
