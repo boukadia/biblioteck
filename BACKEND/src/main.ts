c9d567a1764f85ddb26bf3366aab85823549ff79
boukadia <mustapha.boukadia97@gmail.com>
2026-03-02 10:53:17 +0000

    BIB-61 Changer le Statut :

--- BACKEND/src/main.ts
+++ BACKEND/src/main.ts
@@ -1,8 +1,29 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global prefix for all routes
+  app.setGlobalPrefix('api');
+
+  // Enable CORS
+  app.enableCors({
+    origin: ['http://localhost:3001', 'http://172.16.9.108:3001'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
