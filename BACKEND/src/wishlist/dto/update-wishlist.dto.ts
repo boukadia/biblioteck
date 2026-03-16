73feea61af5adb0dfa38d5a5e73573649bf66d5c
boukadia <mustapha.boukadia97@gmail.com>
2026-03-16 09:32:03 +0000

    BIB-97 Création du DTO de validation

--- BACKEND/src/wishlist/dto/update-wishlist.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsArray, IsInt, IsOptional, IsPositive } from 'class-validator';
-
-export class UpdateWishlistDto {
-    @IsOptional()
-    @IsArray()
-    @IsInt({ each: true })
-    @IsPositive({ each: true })
-    livreIds?: number[];
-}
