a61565f91e6e8a8197cca894d4a8dcc7b5aee994
boukadia <mustapha.boukadia97@gmail.com>
2026-03-16 13:18:43 +0000

    initialer projet et creer structure et  page home

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import  Home  from './pages/Home';
+
+function App() {
+
+ return (
+  <BrowserRouter>
+      <Routes>
+        {/* Route accessible par tout le monde */}
+        <Route path="/" element={<Home />} />
+        
+        {/* Prochaines étapes : Login et Register */}
+        <Route path="/login" element={<div className="container mt-5">Page Login (Bientôt)</div>} />
+        <Route path="/register" element={<div className="container mt-5">Page Register (Bientôt)</div>} />
+      </Routes>
+  </BrowserRouter>
+
+  );
+}
+
+export default App
